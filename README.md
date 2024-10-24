2194da6b2a13b9bb971f58c403578fffe005b861
Pumsuk Cho <pumsuk.cho@appscroft.com>
2024-10-24 18:22:56 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# setup
+# Setup
+
+MacOS 환경에서 초기 세팅 스크립트를 제공한다.
+
+## 설치 방법
+
+```
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ACodeFarmer/setup/HEAD/initialize.sh)"
+```
---
+++ initialize.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Xcode Command Line Tools
+xcode-select --install
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Basic Homebrew packages
+brew install git
